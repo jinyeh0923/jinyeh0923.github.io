3628e46b6084852155da6b686053fc2f67685edb
Jin Yeh <lyrayeh0@gmail.com>
2023-07-20 00:35:19 +0800

    rwd ver3

--- js/script.js
+++ js/script.js
@@ -24,64 +24,8 @@ $(function () {
   });
 });
 
-// 關於我們slide2
-var swiper = new Swiper("#multi_swiper", {
-  slidesPerView: 4,
-  spaceBetween: 25,
-  loop: true,
-  autoplay: {
-    // delay: 2500,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
-  },
-  breakpoints: {
-    320: {
-      slidesPerView: 1,
-      spaceBetween: 10,
-    },
-    768: {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    1024: {
-      slidesPerView: 3,
-      spaceBetween: 20,
-    },
-    1280: {
-      slidesPerView: 4,
-      spaceBetween: 25,
-    },
-  },
-});
-
-// 高爾夫slide
-var swiper = new Swiper("#score_cards_swiper", {
-  loop: true,
-  // effect: "fade",
-  speed: 1500,
-  autoplay: {
-    delay: 2000,
-    disableOnInteraction: false,
-  },
-  pagination: {
-    el: ".swiper-pagination",
-    clickable: true,
-  },
-  // navigation: {
-  //   nextEl: ".swiper-button-next",
-  //   prevEl: ".swiper-button-prev",
-  // },
-});
-
 // gsap
-gsap.registerPlugin(ScrollTrigger);
+// gsap.registerPlugin(ScrollTrigger);
 
 // ScrollTrigger.defaults({
 //     toggleActions: "play",
